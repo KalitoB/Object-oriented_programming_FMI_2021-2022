398536f799be439308b965e2fae7f9c1197289fc
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-13 00:50:46 +0300

    Update Casual.h

--- Additional/Teams/Team 5/src/h/Casual.h
+++ Additional/Teams/Team 5/src/h/Casual.h
@@ -1,1 +1,16 @@
+//Commited By Tanya
 
+#pragma once
+#include "ClothingItem.h"
+
+class Casual : public ClothingItem {
+
+public:
+	Casual(const String& name, const double price, const size_t numAvailable);
+	
+	virtual Type getType() const override;
+	virtual const char* typeToStr() const override;
+	virtual ClothingItem* clone() const override;
+	virtual String getData() const override;
+	virtual void print() const override;
+};
