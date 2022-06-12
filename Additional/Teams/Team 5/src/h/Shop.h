6c25ce543199624c2b033db623f57a9e80ee565e
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-13 00:59:38 +0300

    Update Shop.h

--- Additional/Teams/Team 5/src/h/Shop.h
+++ Additional/Teams/Team 5/src/h/Shop.h
@@ -1,1 +1,32 @@
+//Contributor: Daniela
 
+#include"String.h"
+#include"ClothingItem.h"
+#include"ClothingContainer.h"
+#include<iostream>
+
+class Shop {
+private:
+	String shopName;
+	String address;
+	double profit;
+	ClothingContainer listOfClothes;
+public:
+	Shop();
+
+	String getName() const;
+	String getAddress() const;
+	double getProfit() const;
+
+	void setName(const String&);
+	void setAddress(const String&);
+	void addToProfit(const double);
+
+	void displayMenu();
+	
+private:
+	void diplayAddDialog();
+	void displayListDialog();
+	void displaySellDialog();
+	void displayShopInfo() const;
+};
