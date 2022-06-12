037a320b31d9ccca1d0a18e1ae9da7ca4e7aa8cf
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-13 01:07:21 +0300

    Update Source.cpp

--- Additional/Teams/Team 5/src/cpp/Source.cpp
+++ Additional/Teams/Team 5/src/cpp/Source.cpp
@@ -1,1 +1,22 @@
+//Contributor: Daniela
 
+#include"Shop.h"
+#include"String.h"
+#include<iostream>
+
+int main() {
+	Shop shop;
+	String name;
+	String address;
+	std::cout << "Add shop name: " << std::endl;
+	std::cin >> name;
+	shop.setName(name);
+
+	std::cout << "Add shop address: " << std::endl;
+	std::cin >> address;
+	shop.setAddress(address);
+
+	shop.displayMenu();
+
+	return 0;
+}
