f31d58b941aab819f5f247eacfbd2346ad08e613
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-13 00:58:38 +0300

    Update Couture.h

--- Additional/Teams/Team 5/src/h/Couture.h
+++ Additional/Teams/Team 5/src/h/Couture.h
@@ -1,1 +1,16 @@
+Contributor: Tanya
 
+#pragma once
+#include "ClothingItem.h"
+
+class Couture : public ClothingItem {
+	String designer;
+public:
+	Couture(const String& name, const double price, const size_t numAvailable);
+	void setDesigner(const String&);
+	virtual Type getType() const override;
+	virtual const char* typeToStr() const override;
+	virtual ClothingItem* clone() const override;
+	virtual String getData() const override;
+	virtual void print() const override;
+};
