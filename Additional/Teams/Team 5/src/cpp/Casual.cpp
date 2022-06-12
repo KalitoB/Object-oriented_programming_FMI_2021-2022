e1529c62e1125ba0ac47fbfe5d0e71bee4643dce
KalitoB <98280356+KalitoB@users.noreply.github.com>
2022-06-13 01:02:59 +0300

    Update Casual.cpp

--- Additional/Teams/Team 5/src/cpp/Casual.cpp
+++ Additional/Teams/Team 5/src/cpp/Casual.cpp
@@ -1,1 +1,32 @@
+//Contributors: Tanya and Kalina
 
+#include "Casual.h"
+
+Casual::Casual(const String& name, const double price, const size_t numAvailable) :ClothingItem(name, price, numAvailable)
+{}
+
+ClothingItem::Type Casual::getType() const
+{
+	return ClothingItem::Type::Casual;
+}
+
+
+const char* Casual::typeToStr() const
+{
+	return "Casual";
+}
+
+ClothingItem* Casual::clone() const
+{
+	return new Casual(*this);
+}
+
+String Casual::getData() const
+{
+	return ClothingItem::getData();
+}
+
+void Casual::print() const
+{
+	ClothingItem::print();
+}
